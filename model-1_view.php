64d3e7edd452dde50c43c7b77c2f48b04a3bcb6c
Fernando Bueno <buenofernando13@gmail.com>
2020-08-16 10:53:50 -0300

    Update and rename visualizar.php to model-1_view.php

--- model-1_view.php
+++ model-1_view.php
@@ -1,44 +1,29 @@
 <?php
 // definições de host, database, usuário e senha
 $host = "localhost";
-$db   = "novo";
+$db   = "services";
 $user = "root";
 $pass = "";
 // conecta ao banco de dados
 $con = mysqli_connect($host, $user, $pass) or trigger_error(mysqli_error(),E_USER_ERROR); 
-// seleciona a base de dados em que vamos trabalhar
 mysqli_select_db($con, $db);
 // cria a instrução SQL que vai selecionar os dados
-$query = sprintf("SELECT * FROM `pessoa`");
+$query = sprintf("SELECT * FROM `services`");
 // executa a query
 $dados = mysqli_query($con, $query);
 // transforma os dados em um array
-$linha = mysqli_fetch_assoc($dados);
+$upServices = mysqli_fetch_assoc($dados);
 // calcula quantos dados retornaram
 $total = mysqli_num_rows($dados);
-?>
- 
-<html>
-    <head>
-    <title>Exemplo</title>
-</head>
-<body>
-<?php
     // se o número de resultados for maior que zero, mostra os dados
     if($total > 0) {
         // inicia o loop que vai mostrar todos os dados
         do {
-?>
-            <p><?=$linha['nome']?> / <?=$linha['user']?> - <?=$linha['email']?></p>
-<?php
+            echo $upServices['idServices']." - ".$upServices['item']";
         // finaliza o loop que vai mostrar os dados
-        }while($linha = mysqli_fetch_assoc($dados));
+        }while($upServices = mysqli_fetch_assoc($dados));
     // fim do if 
     }
-?>
-</body>
-</html>
-<?php
 // tira o resultado da busca da memória
 mysqli_free_result($dados);
 ?>
