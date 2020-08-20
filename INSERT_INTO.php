46d4cc84db3647f61899e8c31a3fd545cd4088a9
Fernando Bueno <buenofernando13@gmail.com>
2020-08-20 18:46:42 -0300

    Create INSERT_INTO.php

---
+++ INSERT_INTO.php
@@ -0,0 +1,23 @@
+<?php
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $dbname = "events";
+
+    //Criar a conexao
+    $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
+	date_default_timezone_set('America/Sao_Paulo');
+  $dEvents = date('H:i:s', time());
+  $name = "Mysqli 2020";
+	mysqli_query($conn, "INSERT INTO events (name, date) VALUES ('$name', '$dEvents')")
+    if(mysqli_affected_rows($conn) == 1){ 
+		  echo "success"
+			}else{
+			echo "erro", $conn->error;
+	   }
+	} else {
+            echo "Erro, não possível inserir no banco de dados" , $conn->error;
+    }
+    mysqli_close($conn); //fecha conexão com banco de dados 
+?>
